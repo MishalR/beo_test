763c1036bd948130f8bda7559219a3062125b5db
MishalR <roymishal210@gmail.com>
2020-08-17 11:22:23 +0530

    first commit

---
+++ test_driver/otp_validate_test.dart
@@ -0,0 +1,40 @@
+// This is a basic Flutter Driver test for the application. A Flutter Driver
+// test is an end-to-end test that "drives" your application from another
+// process or even from another computer. If you are familiar with
+// Selenium/WebDriver for web, Espresso for Android or UI Automation for iOS,
+// this is simply Flutter's version of that.
+
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Counter App', () {
+    // First, define the Finders and use them to locate widgets from the
+    // test suite. Note: the Strings provided to the `byValueKey` method must
+    // be the same as the Strings we used for the Keys in step 1.
+
+    final loaderFinder = find.byValueKey('loader');
+
+    FlutterDriver driver;
+
+    // Connect to the Flutter driver before running any tests.
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    // Close the connection to the driver after the tests have completed.
+    tearDownAll(() async {
+      if (driver != null) {
+        driver.close();
+      }
+    });
+
+    test('loader', () async {
+      // First, tap the button.
+      await driver.tap(loaderFinder);
+
+      // Then, verify the counter text is incremented by 1.
+      //expect(await driver.getText(usernameFinder), "Hello!");
+    });
+  });
+}

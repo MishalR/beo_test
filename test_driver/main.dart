aeac5eb9509c16a6b1f11a8e01996e288d2f4c23
MishalR <roymishal210@gmail.com>
2020-08-17 10:12:21 +0530

    first commit

--- test_driver/main.dart
+++ test_driver/main.dart
@@ -1,5 +1,5 @@
 
-import 'package:device_farm_demo/otp_validate.dart';
+import 'package:device_farm_demo/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_driver/driver_extension.dart';
 
@@ -7,5 +7,5 @@ void main() {
   // Enable integration testing with the Flutter Driver extension.
   // See https://flutter.io/testing/ for more info.
   enableFlutterDriverExtension();
-  runApp(OTP());
+  runApp(MyApp());
 }

e3729aaae9a94d9ee48dd7242ce46a800d8d4469
MishalR <roymishal210@gmail.com>
2020-09-11 13:17:42 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -101,9 +101,9 @@ class FormFieldsState extends State<FormFields>
                           ListTile(
                             contentPadding: EdgeInsets.symmetric(horizontal: 0, vertical:0),
                             title:
-                            InkWell(
+                            FlatButton(
                               key: Key('button'),
-                              onTap: () {
+                              onPressed: () {
 
                         Navigator.push(context,
                           MaterialPageRoute(builder: (context) => OTP(),
